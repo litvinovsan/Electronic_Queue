e9aeb6852e0af5a4e60e96f5902fd490c0c00550
Alex <litvinov_san@mail.ru>
2021-02-27 13:52:07 +0500

    Add project files.

---
+++ ElectronicQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
